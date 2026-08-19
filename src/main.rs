b58ed1ec8462806c2df72b3e55f5989e8f211d64
Maaz K <khokharmaaz@gmail.com>
2026-08-19 15:06:15 +0000

    finished proxy

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 use tokio::net::TcpListener;
-use rust_tcp_proxy::{LISTEN_ADDRESS};
+use rust_tcp_proxy::{LISTEN_ADDRESS, proxy};
 
 #[tokio::main]
 async fn main() {
-    let _listener = TcpListener::bind(LISTEN_ADDRESS).await.expect("failed to bind to listen address");
+    let listener = TcpListener::bind(LISTEN_ADDRESS).await.expect("failed to bind to listen address");
 
     println!("Starting server at {LISTEN_ADDRESS}");
 
-    println!("Hello, world!");
+    loop {
+        let (socket, addr) = match listener.accept().await {
+            Ok(t) => t,
+            Err(e) => {
+            println!("failde to accept client connection");
+            eprintln!("{e}");
+            continue
+        }};
+
+        println!("[+] New client connection from {}", addr.ip());
+
+        tokio::spawn(async move {
+            if let Err(e) = proxy(socket).await {
+                println!("failed to proxy client");
+                eprintln!("{e}");
+            };
+        });
+    }
 }
